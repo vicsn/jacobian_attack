f2bdcb8174cb006fcdc28c43681a2c287abb3301
Victor Sint Nicolaas <vicsn@users.noreply.github.com>
2023-12-19 14:32:11 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use snarkvm_curves::templates::short_weierstrass_jacobian::Projective;
+use snarkvm_curves::traits::{AffineCurve, ProjectiveCurve};
+use snarkvm_fields::One;
+use snarkvm_curves::ModelParameters;
+use snarkvm_curves::bls12_377::Bls12_377G1Parameters;
+use snarkvm_fields::Field;
+
+fn main() {
+    let one = <Bls12_377G1Parameters as ModelParameters>::BaseField::one();
+    let two = one + one;
+    let inverse = two.inverse().unwrap();
+    println!("multiplicative inverse test: {:?}", inverse);
+
+    let x_1 = two;
+    let y_1 = two;
+    let z_1 = two;
+
+    let x_2 = two;
+    let y_2 = two;
+    let z_2 = two;
+
+    let mut p1: Projective<Bls12_377G1Parameters> = Projective::new(x_1, y_1, z_1);
+    let p2 = Projective::new(x_2, y_2, z_2);
+
+    p1 += p2;
+    println!("p1: {:?}", p1);
+}
