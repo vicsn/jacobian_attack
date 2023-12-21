c5d4c00f4ce459c024becb6da799b66f277150ff
Victor Sint Nicolaas <vicsn@users.noreply.github.com>
2023-12-21 20:19:52 +0100

    Add test with random point

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use snarkvm_fields::One;
 use snarkvm_curves::ModelParameters;
 use snarkvm_curves::bls12_377::Bls12_377G1Parameters;
 use snarkvm_fields::Field;
+use snarkvm_utilities::TestRng;
+use snarkvm_utilities::rand::Uniform;
 
 fn main() {
     let one = <Bls12_377G1Parameters as ModelParameters>::BaseField::one();
@@ -16,7 +18,7 @@ fn main() {
     let z_1 = two;
 
     let x_2 = two;
-    let y_2 = two;
+    let y_2 = -two;
     let z_2 = two;
 
     let mut p1: Projective<Bls12_377G1Parameters> = Projective::new(x_1, y_1, z_1);
@@ -24,4 +26,10 @@ fn main() {
 
     p1 += p2;
     println!("p1: {:?}", p1);
+
+    let rng = &mut TestRng::default();
+    let mut p3: Projective<Bls12_377G1Parameters> = Projective::rand(rng);
+    let p4 = Projective::new(p3.x, -p3.y, p3.z);
+    p3 += p4;
+    println!("p3: {:?}", p4);
 }
